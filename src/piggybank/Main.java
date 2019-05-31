49662e06e43a021507cad1ff911489a4f872a5e9
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-05-31 10:10:18 -0700

    finished mvp

--- src/piggybank/Main.java
+++ src/piggybank/Main.java
@@ -9,19 +9,18 @@ public class Main {
         // Quarter quarter = new Quarter();
         // System.out.println(quarter.getValue());
         // System.out.println("The piggy bank holds " + fp.format(myValue));
-      
-      
+
         ArrayList<AbstractMoney> piggyBank = new ArrayList<AbstractMoney>();
         piggyBank.add(new Quarter());
         piggyBank.add(new Dime());
         piggyBank.add(new Dollar(5));
-        piggyBank.add( new Nickel(3));
+        piggyBank.add(new Nickel(3));
         piggyBank.add(new Dime(7));
-        piggyBank.add( new Dollar());
+        piggyBank.add(new Dollar());
         piggyBank.add(new Penny(10));
-       
+
         System.out.println("\n*** Display Contents *** \n");
-        piggyBank.forEach((c)-> c.getCoin());
+        piggyBank.forEach((c) -> c.getCoin());
         System.out.println();
         double total = 0;
         for (int i = 0; i < piggyBank.size(); i++) {
@@ -30,7 +29,6 @@ public class Main {
 
         System.out.println("*** Total ***\n");
         System.out.println("The piggy bank holds " + fp.format(total));
-
 
     }
 
