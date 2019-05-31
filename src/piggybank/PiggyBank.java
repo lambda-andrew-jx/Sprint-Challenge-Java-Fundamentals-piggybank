49e5670122b56b69332bd721dc37486861b83971
Thomas-Tuttle <48684599+Thomas-Tuttle@users.noreply.github.com>
2019-05-31 11:54:04 -0400

    Made the bank

---
+++ src/piggybank/PiggyBank.java
@@ -0,0 +1,17 @@
+package piggybank;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+
+public class PiggyBank 
+{
+  static DecimalFormat fp = new DecimalFormat("$###,###.00");
+  private HashMap<String, Integer> cash = new HashMap<>();
+
+  private double total = 0;
+
+  public void add(Cash amount) 
+  {
+   
+  }
+}
