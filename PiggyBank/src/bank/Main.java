3572dc2016f80864b93ddf8a5dc2f6c7eb358fe8
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-05-31 13:24:49 -0400

    Fix bug in Main

--- PiggyBank/src/bank/Main.java
+++ PiggyBank/src/bank/Main.java
@@ -1,9 +1,15 @@
 package bank;
 
+import java.text.DecimalFormat;
+
 
 public class Main {
+
     public static void main(String[] args) 
     {
+
+        DecimalFormat fp = new DecimalFormat("$###,###.00");
+
         Coin dollar = new Coin(1.00);
         Coin quarter = new Coin(0.25);
         Coin dime = new Coin(0.10);
@@ -13,16 +19,25 @@ public class Main {
         // System.out.println("The value of a dollar is " + dollar.getValue());
         // System.out.println("The value of a quarter is " + dime.getValue());
 
-        PiggyBank = new PiggyBank();
+        PiggyBank pb = new PiggyBank();
 
-        pb.addCoins(10, quarter);
-        pb.addCoins(5, penny);
+        pb.addCoins(1, quarter);
+        pb.addCoins(7, dime);
+        pb.addCoins(5, dollar);
+        pb.addCoins(3, nickle);
+        pb.addCoins(7, dime);
+        pb.addCoins(1, dollar);
+        pb.addCoins(10, penny);
+
+
 
         double value = pb.getMoney();
 
         int numcoins = pb.getNumOfCoins();
 
-        // System.out.printf("Coin=%d, Value=%2f\n", numcoins, value);
+        System.out.printf("Coin=%d, Value=%2f\n", numcoins, value);
+
+        System.out.println("The piggy bank holds " + fp.format(sum));
     }
 }
 
