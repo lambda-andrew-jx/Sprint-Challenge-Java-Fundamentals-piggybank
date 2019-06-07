d35a7bbaf68b6f133ea1a9fcf347d4e71e9ee5bf
Sydney Crumley <srsimps19@gmail.com>
2019-06-07 10:51:28 -0500

    built out penny class

--- piggybank/src/piggyBankApp/Penny.java
+++ piggybank/src/piggyBankApp/Penny.java
@@ -1,5 +1,18 @@
 package piggyBankApp;
 
-public class Penny
+public class Penny extends AbstractCoin
 {
+	public Penny(int amount)
+	{
+		super(amount);
+
+		setName("Penny");
+		setValue(1);
+	}
+
+	public Penny()
+	{
+		setName("Penny");
+		setValue(1);
+	}
 }
