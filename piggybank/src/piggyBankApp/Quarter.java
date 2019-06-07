3f46bfba59b424318673935d42f52265f9b36ad9
Sydney Crumley <srsimps19@gmail.com>
2019-06-07 10:52:36 -0500

    built out quarter class

--- piggybank/src/piggyBankApp/Quarter.java
+++ piggybank/src/piggyBankApp/Quarter.java
@@ -1,5 +1,18 @@
 package piggyBankApp;
 
-public class Quarter
+public class Quarter extends AbstractCoin
 {
+	public Quarter(int amount)
+	{
+		super(amount);
+
+		setName("Quarter");
+		setValue(25);
+	}
+
+	public Quarter()
+	{
+		setName("Quarter");
+		setValue(25);
+	}
 }
