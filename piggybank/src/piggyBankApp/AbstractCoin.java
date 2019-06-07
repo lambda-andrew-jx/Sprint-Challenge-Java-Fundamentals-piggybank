703cd971ea217f18979d970dbbd244275d643197
Sydney Crumley <srsimps19@gmail.com>
2019-06-07 10:43:36 -0500

    built out Abstract Coin class

--- piggybank/src/piggyBankApp/AbstractCoin.java
+++ piggybank/src/piggyBankApp/AbstractCoin.java
@@ -1,5 +1,76 @@
 package piggyBankApp;
 
-public class AbstractCoin
+public abstract class AbstractCoin
 {
+	private String name;
+	private int value;
+	private int amount;
+
+	public AbstractCoin()
+	{
+		this.amount = 1;
+	}
+
+	public AbstractCoin(int amount)
+	{
+		this.amount = amount;
+	}
+
+	public int getTotal()
+	{
+		return this.value * this.amount;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getValue()
+	{
+		return this.value;
+	}
+
+	public void setValue(int value)
+	{
+		this.value = value;
+	}
+
+	public int getAmount()
+	{
+		return this.amount;
+	}
+
+	public void setAmount(int amount)
+	{
+		this.amount = amount;
+	}
+
+	@Override
+	public String toString()
+	{
+		if (this.name == "Dollar") {
+			return "$" + this.amount;
+		}
+		else if (this.amount > 1 )
+		{
+			if (this.name == "Penny")
+			{
+				return this.amount + " Pennies";
+			}
+			else
+			{
+				return this.amount + " " + this.name + "s";
+			}
+		}
+		else
+		{
+			return this.amount + " " + this.name;
+		}
+	}
 }
