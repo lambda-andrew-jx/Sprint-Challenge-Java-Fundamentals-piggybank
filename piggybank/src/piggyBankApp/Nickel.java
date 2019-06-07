991e2c80b0d9d56f3016ed7e4707b7d9f4016908
Sydney Crumley <srsimps19@gmail.com>
2019-06-07 10:49:54 -0500

    buuilt out nickel class

--- piggybank/src/piggyBankApp/Nickel.java
+++ piggybank/src/piggyBankApp/Nickel.java
@@ -1,5 +1,18 @@
 package piggyBankApp;
 
-public class Nickel
+public class Nickel extends AbstractCoin
 {
+	public Nickel(int amount)
+	{
+		super(amount);
+
+		setName("Nickel");
+		setValue(5);
+	}
+
+	public Nickel()
+	{
+		setName("Nickel");
+		setValue(5);
+	}
 }
