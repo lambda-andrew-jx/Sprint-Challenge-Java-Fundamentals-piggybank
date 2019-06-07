e8da044a1b6467e7d327f1becd5e6c431ce6d37c
Sydney Crumley <srsimps19@gmail.com>
2019-06-07 10:35:58 -0500

    built out most of main class

--- piggybank/src/piggyBankApp/Main.java
+++ piggybank/src/piggyBankApp/Main.java
@@ -1,5 +1,33 @@
 package piggyBankApp;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
 public class Main
 {
+	public static void printTotal(ArrayList<AbstractCoin> bank)
+	{
+		DecimalFormat fp = new DecimalFormat("###,###.00");
+
+		double total = 0;
+
+		for (AbstractCoin c : bank)
+		{
+			total += c.getTotal();
+		}
+
+		System.out.println("The piggy bank has " + fp.format(total/100));
+	}
+	public static void main(String[] args)
+	{
+		ArrayList<AbstractCoin> piggybank = new ArrayList<AbstractCoin>();
+
+		piggybank.add(new Quarter());
+		piggybank.add(new Dime());
+		piggybank.add(new Dollar());
+		piggybank.add(new Nickel());
+		piggybank.add(new Dime());
+		piggybank.add(new Dollar());
+		piggybank.add(new Penny());
+	}
 }
